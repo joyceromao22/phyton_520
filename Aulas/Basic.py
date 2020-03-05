ad464b3b437df71b8155468670661e21126d4baa
joyce romao <joyceromao22@gmail.com>
2020-03-04 22:26:06 -0300

    aula01

---
+++ Aulas/Basic.py
@@ -0,0 +1,16 @@
+#-*- codingÇ utf-8 -*-
+
+nome = 'Joyce'
+idade = 29
+altura = 1.63
+brasileiro = True
+
+#print ('ola meu nome e',nome) #primeira opcao
+#print('ola meu nome e {} {}'.format(nome), (idade)) #segunda opcao
+print( f'ola meu nome e {nome}') #terceira opcao
+
+#vendo tipos de dados
+# print (type(nome))
+# print (type(idade))
+# print (type(altura))
+# print (type(brasileiro))
